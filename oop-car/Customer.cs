a17b5a0a9ff454a686036de2dd6eea3294b90efb
Arcadios-cr <55406273+Arcadios-cr@users.noreply.github.com>
2021-02-22 15:24:53 +0100

    oop

---
+++ oop-car/Customer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop_car
+{
+    class Customer
+    {
+        public double hour { get; set; }
+        public double leave { get; set; }
+
+        public double price { get; set; }
+
+        public Customer(double _hour, double _leave, double _price)
+        {
+            this.hour = _hour;
+            this.leave = _leave;
+            this.price = _price;
+        }
+
+
+        public double calculateCharges()
+        {
+            if(leave-hour <= 3)
+            {
+                Console.WriteLine($"You must pay 2$");
+                price = 2;
+            }
+            else
+            {
+                if(leave-hour == 24)
+                 {
+                        Console.WriteLine($"You must pay 10$");
+                    price = 10;
+                 }
+                else
+                {
+                        double value = 2 + ((leave - hour)-3) * 0.50;
+                        price = value;
+                    if (price > 10)
+                        price = 10;
+                        Console.WriteLine($"You must pay {price} $");
+                    }
+                }
+
+            return price;
+        }
+    }
+}
---
+++ oop-car/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+
+namespace oop_car
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer mycustomer = new Customer(6.30, 10.30, 0);
+            Customer mycustomer2 = new Customer(0, 23, 0);
+            Customer mycustomer3 = new Customer(9.30, 10.30, 0);
+            double total = mycustomer.calculateCharges()+ mycustomer2.calculateCharges()+ mycustomer3.calculateCharges();
+            Console.WriteLine($"Total = {total}");
+        }
+    }
+}
